daddad0d39743c911aab9b59146e2ea0f456c31e
mgechev <mgechev@gmail.com>
2019-07-30 16:44:11 -0700

    Export LinkDirective as private

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { QuicklinkModule } from './quicklink.module';
+export { LinkDirective as ɵɵLinkDirective } from './link.directive';
 export { QuicklinkStrategy } from './quicklink-strategy.service';
