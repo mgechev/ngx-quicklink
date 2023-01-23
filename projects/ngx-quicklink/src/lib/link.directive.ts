a8fa22a41b7829c0820e3a861dbdf6cd73268e34
Koen François <koen.francois@showpad.com>
2023-01-23 21:08:42 +0100

    Run Angular 15 update-script

--- projects/ngx-quicklink/src/lib/link.directive.ts
+++ projects/ngx-quicklink/src/lib/link.directive.ts
@@ -8,50 +8,49 @@ import {
   Input,
   SimpleChanges
 } from '@angular/core';
-import { RouterLink, RouterLinkWithHref } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { LinkHandler } from './link-handler.service';
 import { LinkHandlerStrategy } from './link-handler-strategy';
 
 @Directive({
-  selector: '[routerLink]'
+    selector: '[routerLink]'
 })
 export class LinkDirective implements OnChanges, OnDestroy {
-  @Input() routerLink: Pick<RouterLink | RouterLinkWithHref, 'routerLink'>['routerLink'];
-  private rl: RouterLink | RouterLinkWithHref;
-  private linkHandler: LinkHandlerStrategy | undefined;
+    @Input() routerLink: Pick<RouterLink, 'routerLink'>['routerLink'];
+    private rl: RouterLink;
+    private linkHandler: LinkHandlerStrategy | undefined;
 
-  constructor(
-    @Inject(LinkHandler) private linkHandlers: LinkHandlerStrategy[],
-    private el: ElementRef,
-    @Optional() link: RouterLink,
-    @Optional() linkWithHref: RouterLinkWithHref
-  ) {
-    this.linkHandler = this.linkHandlers.filter(h => h.supported()).shift();
-    this.rl = link || linkWithHref;
-    if (this.element && this.element.setAttribute) {
-      this.element.setAttribute('ngx-ql', '');
+    constructor(
+        @Inject(LinkHandler) private linkHandlers: LinkHandlerStrategy[],
+        private el: ElementRef,
+        @Optional() link: RouterLink,
+    ) {
+        this.linkHandler = this.linkHandlers.filter(h => h.supported()).shift();
+        this.rl = link;
+        if (this.element && this.element.setAttribute) {
+            this.element.setAttribute('ngx-ql', '');
+        }
     }
-  }
 
-  ngOnChanges(c: SimpleChanges) {
-    if (c['routerLink'] && this.linkHandler) {
-      this.linkHandler.unregister(this);
-      this.linkHandler.register(this);
+    ngOnChanges(c: SimpleChanges) {
+        if (c['routerLink'] && this.linkHandler) {
+            this.linkHandler.unregister(this);
+            this.linkHandler.register(this);
+        }
     }
-  }
 
-  ngOnDestroy() {
-    if (!this.linkHandler) {
-      return;
+    ngOnDestroy() {
+        if (!this.linkHandler) {
+            return;
+        }
+        this.linkHandler.unregister(this);
     }
-    this.linkHandler.unregister(this);
-  }
 
-  get element(): Element {
-    return this.el.nativeElement;
-  }
+    get element(): Element {
+        return this.el.nativeElement;
+    }
 
-  get urlTree(): any {
-    return this.rl.urlTree;
-  }
+    get urlTree(): any {
+        return this.rl.urlTree;
+    }
 }
