1bcef51dab2a9eb9009b03832d20d003fc300b68
Clara Castillo <claracastillochale@gmail.com>
2023-02-18 00:26:29 +0100

    Export quicklinkProviders
    `quicklinkProviders` export was missing. It is required in standalone applications.

--- projects/ngx-quicklink/src/public-api.ts
+++ projects/ngx-quicklink/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-quicklink
  */
 
+export { quicklinkProviders } from './lib/quicklink.module';
 export { QuicklinkModule } from './lib/quicklink.module';
 export { LinkDirective as QuicklinkDirective } from './lib/link.directive';
 export { QuicklinkStrategy } from './lib/quicklink-strategy.service';
