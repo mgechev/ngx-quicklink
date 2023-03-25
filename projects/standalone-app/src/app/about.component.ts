9d6eb92cb7f04b601d4765908c64983fa8366fdd
Nils Tijtgat <nils.tijtgat@showpad.com>
2023-03-25 14:45:33 +0100

    Fix preloading in standalone applications with nested routes

--- projects/standalone-app/src/app/about.component.ts
+++ projects/standalone-app/src/app/about.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { QuicklinkDirective } from 'ngx-quicklink';
 
 @Component({
   standalone: true,
+  imports: [RouterModule, QuicklinkDirective],
   template: `
     About component
+
+    <a [routerLink]="['/', 'about', 'sub']">SUB</a>
+
+    <router-outlet></router-outlet>
   `,
 })
 export default class AboutComponent {}
--- projects/standalone-app/src/app/app.component.ts
+++ projects/standalone-app/src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { QuicklinkDirective } from 'ngx-quicklink';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  styleUrls: ['./app.component.less'],
+  imports: [RouterModule, QuicklinkDirective],
   template: `
+    <h1>Standalone Example</h1>
+
+    <div class="navigation">
+      <a routerLink="['/']">Root</a>
+      <a [routerLink]="primaryRoutePath">Home</a>
+      <a [routerLink]="['/', 'about']">About</a>
+      <a [routerLink]="secondaryRoutePath">Social</a>
+    </div>
+
     <router-outlet></router-outlet>
+
+    <div class="side">
+      Side Outlet
+      <router-outlet name="side"></router-outlet>
+    </div>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  primaryRoutePath: any[] = [];
+  secondaryRoutePath: any[] = [];
+
+  constructor() {
+    // The timeouts are used to demonstrate dynamically preloading routes
+    // Check the network tab to see this in action!
+    setTimeout(() => {
+      this.primaryRoutePath = ['/', 'home'];
+      console.log('Home route added');
+    }, 3000);
+    setTimeout(() => {
+      this.secondaryRoutePath = ['/', { outlets: { side: ['social'] } }];
+      console.log('Social route added');
+    }, 2000);
+  }
+}
