63a36218fb5c46a91c0f53f696ee9ef871cd66d7
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-05-04 17:24:36 +0200

    feat: :arrow_up: migrate to Angular 16 (#152)
    * feat: :arrow_up: migrate to Angular 16
    
    * chore: :arrow_up: upgrade Typescript dependencies to last version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -24,12 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('csr');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('csr app is running!');
   });
 });
