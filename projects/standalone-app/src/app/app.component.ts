d8c2f6fcf5dc67b0561f661781d47165637788e1
mgechev <mgechev@gmail.com>
2025-12-05 12:28:26 -0800

    chore: update to v19

--- projects/standalone-app/src/app/app.component.ts
+++ projects/standalone-app/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterModule],
-  template: `
+    selector: 'app-root',
+    imports: [RouterModule],
+    template: `
     <router-outlet></router-outlet>
-  `,
+  `
 })
 export class AppComponent {}
